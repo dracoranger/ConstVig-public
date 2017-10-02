9ba7c87a71626fd99e2ebb59b209b9185a5c8f44
Bowers <1501539992@mil>
2017-10-02 10:40:24 -0400

    V0.0
    Initial commit
    Creates a few functions in main
    Creates the high level files of fileSystem, networkSystem, and timingSystem

---
+++ main.py
@@ -0,0 +1,28 @@
+'''
+main.py
+'''
+
+'''
+main function
+
+
+'''
+def main():
+
+
+'''
+error catching function
+
+
+'''
+def error():
+
+
+'''
+logging function:
+
+
+'''
+def log(inpu):
+    if(type(inpu)!=type('str')):
+        #FAIL RETURN TO START!
