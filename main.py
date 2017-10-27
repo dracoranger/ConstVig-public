91531d6dda36a8b11b55aa199bcdf274b66815c4
AKronenberg <abigail.kronenberg@usma.edu>
2017-10-27 10:25:08 -0400

    cleaned up main.py

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
 '''
 main.py
-'''
 
-'''
 Creator:
 Input:
 Output:
 This function
 '''
 
-#import statements
-#Utilities function
-#time
-#easysockets?
 import time
-import timeit
 import os
 import subprocess
 import utilities
-
+#Utilities function
+#easysockets?
 
 #any global constants- there should be none
 #might need log file location
@@ -35,8 +29,8 @@ class TARGET: #target data, such as IP and type
     This function takes in targeting formation and stores it for use later
     just an intention at the moment
     '''
-    def __init__(self, PLACEHOLDER):
-        self.ip = 0 #make attack vars, need to make integrate well with networking pieces
+    def __init__(self, placeholder):
+        self.ip_addr = 0 #make attack vars, need to make integrate well with networking pieces
 #targets, array of target?
 
 
@@ -159,20 +153,24 @@ def main():
     sync timer?  Probably not necessary on same system
 
     '''
-    SETTINGS = '' #other settings stuff, placeholder currently
-    DEATH_LIMIT = 5
-    ROUND_LENGTH = [300,300,300,300,300]#array of round lengths
-    ROUND_NUMBER = 0
-    SAFETY_BUFFER = 10
-    TIME_BETWEEN_CHECK = 5
+    settings = '' #other settings stuff, placeholder currently
+    death_limit = 5
+    round_length = [300, 300, 300, 300, 300]#array of round lengths
+    round_number = 0
+    safety_buffer = 10
+    time_between_check = 5
     global LOG_FILE
     global SETTING
 
-    SETTINGS = [PATH, SETTINGS, DEATH_LIMIT, ROUND_LENGTH, ROUND_NUMBER, TIME_BETWEEN_CHECK, LOG_FILE]
+    settings = [PATH, settings, death_limit, round_length,
+                round_number, time_between_check, LOG_FILE]
 
-    ROUND_LENGTH, ROUND_NUMBER, TARGETS, ADDITONAL = parse_settings(PATH, SETTINGS)#additional is a temp name here
+    round_length, round_number, targets, additional = parse_settings(PATH,
+                                                                     settings)
+    #additional is a temp name here
 
-    #setSettings = parse_settings(ADDITONAL)#changes the settings outside the major timing results and attacking method
+    #setSettings = parse_settings(additional)
+    #changes the settings outside the major timing results and attacking method
 
     if LOG_FILE == "":
         LOG_FILE = set_log_file(PATH)
@@ -180,46 +178,50 @@ def main():
         if success != 1:
             print("log failure")
 
-    childMaster = [CHILD('ChildFS'), CHILD('ChildNI')]
-    childMaster.append(CHILD('ChildNO'))
-    childMaster.append(CHILD('ChildUI'))
+    childmaster = [CHILD('ChildFS'), CHILD('ChildNI')]
+    childmaster.append(CHILD('ChildNO'))
+    childmaster.append(CHILD('ChildUI'))
 
-    #childHackArray=[0]# push down to network outgoing? Stores each hack, ensures that failing hack only kills itself, not everything.
+    #childHackArray=[0]
+    # push down to network outgoing? Stores each hack,
+    # ensures that failing hack only kills itself, not everything.
 
 
-    for i in range(0, ROUND_NUMBER):
+    for i in range(0, round_number):
         time_start = time.time()
         time_rec = time.time()
-        while time.time() - time_start < ROUND_LENGTH[i] + SAFETY_BUFFER:
+        while time.time() - time_start < round_length[i] + safety_buffer:
 
             #wait on user input, returns from NI or NO
             #push information to logging
-            #TODO make it so that if child is alive, actually make child.is_alive true
+            #Todo make it so that if child is alive, actually make child.is_alive true
             #print(str(time.time()-time_start))
-            if time.time()-time_rec >= TIME_BETWEEN_CHECK:
+            if time.time()-time_rec >= time_between_check:
                 print(str(time.time()-time_start))
-                for child in childMaster:
+                for child in childmaster:
                     #if child.process.poll() == None:
                     if isinstance(child.process.poll(), type(None)):
                         child.update_is_alive(True)
                     outpu = ''
                     inpu = 'send/recieve'#need to figure out how to replace with necessary data
                 #for num in range (0, CHILD_NUM):
-                    #if childMaster[num].:
+                    #if childmaster[num].:
                     if child.is_alive():
                         temp = child.get_name() + ' is alive!'
                         print(temp)
                         inpu = get_input()
                         outpu = child.process.communicate(inpu)#should communicate with the process
                         print(outpu)
-                        #TODO make outpu useful
+                        #Todo make outpu useful
                     elif child.is_keep_running():
                         child.recreate_subprocess()
                         child.inc_deaths()
-                        temp = child.get_name()+" has died. Total deaths for "+ child.get_name()+": "+str(child.get_deaths())
+                        temp = child.get_name()+''' has died.
+                        Total deaths for '''+ child.get_name()+": "+str(child.get_deaths())
                         log(temp)
-                        if child.get_deaths() > DEATH_LIMIT:
-                            temp = child.get_name()+" has died "+ str(child.get_deaths()) +" times. Continue anyways (y/n)?"
+                        if child.get_deaths() > death_limit:
+                            temp = child.get_name()+''' has
+                             died '''+ str(child.get_deaths()) +" times. Continue anyways (y/n)?"
                             cont = input(temp)
                             print(cont)
                             if cont[:1].upper() == "N":
@@ -231,9 +233,11 @@ def main():
         print('round '+str(i)+' complete')
     print('fully complete')
 
-    #round length is minutes? seconds? per round, and controls how often the NO runs, and how often NI detects
+    #round length is minutes? seconds? per round,
+    # and controls how often the NO runs, and how often NI detects
     #round number determines the number of rounds
-    #Target num data structure that contains information on who to attack, who not to attack, expected operating systems, attacks to ignore, more data will be there.
+    #Target num data structure that contains information on who to attack,
+    # who not to attack, expected operating systems, attacks to ignore, more data will be there.
 
 def log(inpu):
     '''
@@ -245,7 +249,7 @@ def log(inpu):
 
     '''
     ret = -1
-    if utilities.check_input('str',inpu):
+    if utilities.check_input('str', inpu):
         temp = open(PATH+'\\'+LOG_FILE, "r+")#should take care of the LOG_FILE not being created
         temp.write(inpu+'\n')
         temp.close()
@@ -258,7 +262,8 @@ def gui_minus():
     Creator: Tate Bowers
     Input: none
     Output: none
-    This function acts as the debugging function while the actual user input child is being generated
+    This function acts as the debugging function while the actual user
+     input child is being generated
     '''
     return ''
 
@@ -288,7 +293,7 @@ def parse_settings(path, name):
     TODO: properly parse settings file, need to talk with group to go over it
     '''
     current_setting = ''
-    PLACEHOLDER = ''
+    placeholder = ''
     make_happy = 0
     ret = [[40, 40, 40, 40, 40], 5, '', '']
     if utilities.check_input('str', path):
@@ -297,10 +302,10 @@ def parse_settings(path, name):
             for i in fil.read():
                 if current_setting == '':
                     current_setting = i[:-1]
-                elif current_setting == PLACEHOLDER:
+                elif current_setting == placeholder:
                     #do stuff
                     make_happy = make_happy + 1
-                elif current_setting == PLACEHOLDER:
+                elif current_setting == placeholder:
                     #do stuff
                     make_happy = make_happy + 1
                 else:
