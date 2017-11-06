3f2702837ed9675d953c89f514fc899549d3c579
AKronenberg <abigail.kronenberg@usma.edu>
2017-11-06 11:32:17 -0500

    V0.17.2

--- generateTargets.py
+++ generateTargets.py
@@ -1,13 +1,15 @@
-'''
+"""
 generate_settings.py
 
-creates a csv containing the number of rounds, the length of the rounds, the target data, and other settings
-'''
+creates a csv containing the number of rounds,
+ the length of the rounds, the target data, and other settings
+"""
 
 def main():
+    """TODO -- create docstring"""
     #prompt user for number and length of rounds
     #allow user to create many identical or create a list of invdividual rounds
-
+    pass
     #prompt user for TARGETS
     #allow to do range of IP addresses
     #allow to do mac addresses ?
@@ -15,3 +17,5 @@ def main():
 #def rounds(): do we need to do this?
 
 def target(): #creates datastructure that holds targeting information
+    """TODO -- create docstring"""
+    pass
