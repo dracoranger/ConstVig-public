cf146618cf4bce36b4e5381e7c114d2b4db5540e
Kamalu76 <Jaryn.villegas@usma.edu>
2017-12-11 09:55:05 -0500

    send pcap
    send pcap files via network to conduct analysis.

--- NI.py
+++ NI.py
@@ -33,11 +33,13 @@ class tcp(object):
             print('Socket connects', soc.getsockname(), 'and', soc.getpeername())
             message = soc.recv(16)
             message_length = int(message.split()[1].decode('utf-8'))
-            return_message = message
+            #return_message = message
             rest = soc.recv(message_length)
-            return_message += rest
-            print_message = return_message.decode('utf-8')
-            print('The incoming sixteen-octet message says', print_message)
+            return_message = rest.split(2)
+            #print_message = return_message.decode('utf-8')
+            with open('test.pcap','wb') as w:
+                w.write(return_message)
+            print('Recieved new capture!')#print('The incoming sixteen-octet message says', print_message)
             ms = '\nGot The Message!!!'
             soc.sendall(ms.encode())
             soc.close()
@@ -63,10 +65,16 @@ class tcp(object):
         sock.close()
 
 def random_msg():
-    size = random.randint(17000,21000)
+    with open(r'C:\Users\x86075\Documents\Firstie Year\IT401\Sprint2\17-36.pcap','rb') as read:
+        lines = read.readlines()
+        part = b''
+        for line in lines[:10]:
+            part+=line
+    size = len(part)#random.randint(17000,21000)
+    print(size)
     messageHeader = "Length: " + str(size +10) +"\r\n\r\n"
     messageBody = ''.join([random.choice(string.ascii_letters + string.digits) for x in range(size)])
-    message = messageHeader + messageBody
+    message = messageHeader.encode() + part#messageBody
     return message#.encode('ascii')
 
 if __name__=='__main__':
