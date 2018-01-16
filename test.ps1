b4aafc8f52d0f829d5468e161cb810a710930b70
gabeyarbro <gabriel.yarbrough@usma.edu>
2018-01-16 11:21:40 -0500

    Rawr

---
+++ test.ps1
@@ -0,0 +1,1 @@
+tshark -r C:\Users\x85491\Documents\2017-2018\IT401\Sprint2\test1.pcap -T fields -e frame.number -e frame.time -e eth.src -e eth.dst -e ip.src -e ip.dst -e ip.proto -e tcp.srcport -e tcp.dstport -E header=y -E separator=, -E quote=d -E occurrence=f > C:\Users\x85491\Documents\2017-2018\IT401\Sprint2\test1.csv
