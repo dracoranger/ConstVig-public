e0919391de0ffab65c2a53dca35a36e4847918a7
Kronenberg <1501767243@mil>
2018-01-26 10:34:51 -0500

    tShark improves

--- Code/childNO.py
+++ Code/childNO.py
@@ -34,54 +34,12 @@ attackDictionary = {
 
 
 }
+attackOrder=[]
 chaffDictionary = {
 
 
 }
-
-#generalized version of below
-def iter_thru_config(which, dicti):
-    #which is either "Attack" or "Chaff"
-    diction = utilities.parseConfig(which)
-    for i in diction:
-        if not i in dicti:
-            #dicti is either chaff or attack dictionary
-            dicti[i] = diction[i]
-
-def run_processes(which, dicti, path):
-    #which is either "Attack" or "Chaff"
-    iter_thru_config(which, dicti)
-    #either PATH_ATTACK or PATH_CHAFF
-    directory = os.fsencode(path)
-    launchStorage = [] #inefficent as heck.  Not sure how else to guarentee aphebetical and no duplicates
-    launchOrder = []
-    for fil in os.listdir(directory):
-        filename = os.fsdecode(fil)
-        #print(attackDictionary)
-        run = dicti[filename]
-        run = run.replace(filename, PATH_ATTACK+'\\'+filename)
-        launch = utilities.create_child_gen(run)
-        launchStorage.append(launch)
-        launchOrder.append(filename)
-
-    complete = False
-    while not complete:
-        #might want to create an escape which kills an indefinitely running process
-        currNum = 0
-        time.sleep(3)
-        complete = True
-        for launch in launchStorage:
-            if utilities.check_input(launch.poll(),1):
-                if launch.poll() == 0: #think this should work.  Not sure since not a child class.  Might just be process.poll
-                    print(str(launchOrder[currNum]) + ' success: '+str(launch.poll()))
-                    #push to logfile success
-                else:
-                    print(str(launchOrder[currNum])+ str(launch.poll()))#+attack.process.stderr)
-            elif isinstance(launch.poll(), type(None)):
-                print(launchOrder[currNum]+' on going')
-                complete = False
-            currNum = currNum+1
-
+chaffOrder=[]
 #generates attackDictionary
 #generates order
 def iter_thru_attack_config():
@@ -94,7 +52,6 @@ def run_attacks():
     iter_thru_attack_config()
     directory = os.fsencode(PATH_ATTACK)
     attackStorage = [] #inefficent as heck.  Not sure how else to guarentee aphebetical and no duplicates
-    attackOrder = []
     for fil in os.listdir(directory):
         filename = os.fsdecode(fil)
         run = attackDictionary[filename]
@@ -105,7 +62,6 @@ def run_attacks():
 
     complete = False
     while not complete:
-        #might want to create an escape which kills an indefinitely running process
         currNum = 0
         time.sleep(3)
         complete = True
@@ -123,7 +79,6 @@ def run_attacks():
 
 
 def run_chaff():
-    # use generalized
     return "incomplete"
 
 def main():
@@ -136,12 +91,7 @@ def main():
     Exceptions raised:
     Restrictions on when it can be called:
     """
-    #Either preferences in main or here
-    #read_preferences()
-    #which, dicti, path
-    run_processes("Chaff",chaffDictionary,PATH_CHAFF)
-    run_processes("Attacks",attackDictionary,PATH_ATTACK)
-    run_processes("Chaff",chaffDictionary,PATH_CHAFF)
+    #loop through on a user-established timer interval
+    run_attacks()
         #commented out the print() and input()
         #print('NO')
-main()
--- tShark.py
+++ tShark.py
@@ -8,43 +8,46 @@ for dir_name in os.walk(tgt_dir):
 	if 'put_pcaps_here' == os.path.basename(dir_name[0]):
 		pcap_dir = dir_name[0]
 
-fname = pcap_dir + "\\test1.pcap"
-tsharkCall1 = [
-				"tshark",
-				"-r",
-				fname,
-				"-T",
-				"fields",
-				"-e",
-				"frame.time_epoch"
-]
-namer = subprocess.Popen(tsharkCall1, shell=True, stdout=subprocess.PIPE)
-tsharkCall2 = [
-				"tshark",
-				"-r",
-				fname,
-				#"C:\\Users\\x85491\\Documents\\2017-2018\\IT401\\Sprint3\\test1.pcap", #File path of pcap
-				"-T",
-				"fields",
-				"-e",
-				"frame.number",
-				"-e",
-				"frame.time_epoch",
-				"-e",
-				"tcp.srcport",
-				"-e",
-				"tcp.dstport",
-				"-E",
-				"header=y",
-				"-E",
-				"separator=/t",
-				"-E",
-				"quote=d",
-				"-E",
-				"occurrence=f",
-				">",
-				pcap_dir + "\\testWS.txt"
-				#"C:\\Users\\x86991\\Documents\\XE402\\ConstVig\\put_pcaps_here\\test1.pcap" #File path for txt file written
-				]
-process = subprocess.Popen(tsharkCall2, shell=True, stdout=subprocess.PIPE)
-process.wait()
+for fil in os.listdir(pcap_dir):
+	sname = os.fsdecode(fil)
+	fname = pcap_dir+"\\"+sname
+	fbase = sname.split(".")[0]
+	newf = fbase + ".csv"
+	tsharkCall1 = [
+					"tshark",
+					"-r",
+					fname,
+					"-T",
+					"fields",
+					"-e",
+					"frame.time_epoch"
+	]
+	namer = subprocess.Popen(tsharkCall1, shell=True, stdout=subprocess.PIPE)
+	tsharkCall2 = [
+					"tshark",
+					"-r",
+					fname,
+					"-T",
+					"fields",
+					"-e",
+					"frame.number",
+					"-e",
+					"frame.time_epoch",
+					"-e",
+					"tcp.srcport",
+					"-e",
+					"tcp.dstport",
+					"-E",
+					"header=y",
+					"-E",
+					"separator=,",
+					"-E",
+					"quote=d",
+					"-E",
+					"occurrence=f",
+					">",
+					pcap_dir +"\\"+newf
+	]
+	process = subprocess.Popen(tsharkCall2, shell=True, stdout=subprocess.PIPE)
+	process.wait()
+	os.unlink(fname)
