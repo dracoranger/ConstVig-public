ddbc5f57b6c86331d9906d636d895d41c848c916
Tate <dracoranger@hotmail.com>
2018-01-26 11:53:55 -0500

    Network out now creates a log of attack output

--- Code/childNO.py
+++ Code/childNO.py
@@ -26,20 +26,87 @@ NetworkOut
 #Path variables
 #os.path.dirname(os.path.realpath(__file__)) or os.getcwd()
 #https://stackoverflow.com/questions/5137497/find-current-directory-and-files-directory
-PATH_ATTACK = os.getcwd()+'\\attacks'
-PATH_CHAFF = os.getcwd()+'\\chaff'
+diction = utilities.parseConfig('Attacks')
+PATH_ATTACK = diction['path_attack']
+PATH_CHAFF = diction['path_chaff']
+IP_RANGE = diction['ipRange']
+PORTS = diction['ports']
+SUBMIT_FLAG_PORT = diction['flag_ports']
+SUBMIT_FLAG_IP = diction['flag_ports']
 
 # filename : running arugments stored as a list
 attackDictionary = {
 
 
 }
-attackOrder=[]
 chaffDictionary = {
 
 
 }
-chaffOrder=[]
+
+def send_flag(flag):
+
+
+#generalized version of below
+def iter_thru_config(which, dicti):
+    #which is either "Attack" or "Chaff"
+    diction = utilities.parseConfig(which)
+    for i in diction:
+        if not i in dicti:
+            #dicti is either chaff or attack dictionary
+            dicti[i] = diction[i]
+
+def run_processes(which, dicti, path, log):
+    #which is either "Attack" or "Chaff"
+    iter_thru_config(which, dicti)
+    #either PATH_ATTACK or PATH_CHAFF
+    directory = os.fsencode(path)
+    launchStorage = [] #inefficent as heck.  Not sure how else to guarentee aphebetical and no duplicates
+    launchOrder = []
+    for fil in os.listdir(directory):
+        filename = os.fsdecode(fil)
+        #print(attackDictionary)
+        run = dicti[filename]
+        run = run.replace(filename, PATH_ATTACK+'\\'+filename)
+        #allow replacement of IP and Ports accessed
+        #might chose how flag is sent.
+        #if not run.find('-i') == -1:
+
+        #if not run.find('-p') == -1:
+
+        #if not
+
+        launch = utilities.create_child_gen(run)
+        launchStorage.append(launch)
+        launchOrder.append(filename)
+
+    complete = False
+    while not complete:
+        #might want to create an escape which kills an indefinitely running process
+        remove = []
+        currNum = 0
+        time.sleep(3)
+        complete = True
+        for launch in launchStorage:
+            if utilities.check_input(launch.poll(),1):
+                if launch.poll() == 0: #think this should work.  Not sure since not a child class.  Might just be process.poll
+                    log = open(log, 'a')
+                    log.write(str(launchOrder[currNum]) + ' success: '+str(launch.communicate())+ '\n')
+                    log.close()
+                    #print(str(launchOrder[currNum]) + ' success: '+str(launch.communicate()))
+                    remove.append(launch)
+                    #push to logfile success
+                else:
+                    print(str(launchOrder[currNum])+ str(launch.communicate()))#+attack.process.stderr)
+            elif isinstance(launch.poll(), type(None)):
+                print(launchOrder[currNum]+' on going')
+                complete = False
+            currNum = currNum+1
+        for i in remove:
+            temp = launchStorage.index(i)
+            launchStorage.remove(i)
+            launchOrder.pop(temp)
+
 #generates attackDictionary
 #generates order
 def iter_thru_attack_config():
@@ -52,6 +119,7 @@ def run_attacks():
     iter_thru_attack_config()
     directory = os.fsencode(PATH_ATTACK)
     attackStorage = [] #inefficent as heck.  Not sure how else to guarentee aphebetical and no duplicates
+    attackOrder = []
     for fil in os.listdir(directory):
         filename = os.fsdecode(fil)
         run = attackDictionary[filename]
@@ -62,6 +130,7 @@ def run_attacks():
 
     complete = False
     while not complete:
+        #might want to create an escape which kills an indefinitely running process
         currNum = 0
         time.sleep(3)
         complete = True
@@ -79,6 +148,7 @@ def run_attacks():
 
 
 def run_chaff():
+    # use generalized
     return "incomplete"
 
 def main():
@@ -91,7 +161,12 @@ def main():
     Exceptions raised:
     Restrictions on when it can be called:
     """
-    #loop through on a user-established timer interval
-    run_attacks()
+    #Either preferences in main or here
+    #read_preferences()
+    #which, dicti, path
+    run_processes("Chaff",chaffDictionary,PATH_CHAFF, 'chaff.log')
+    run_processes("Attacks",attackDictionary,PATH_ATTACK, 'attack.log')
+    run_processes("Chaff",chaffDictionary,PATH_CHAFF, 'chaff.log')
         #commented out the print() and input()
         #print('NO')
+main()
