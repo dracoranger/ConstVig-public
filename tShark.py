0395c9dd76c2dea511b0b6138d6ef2e4c9272cab
Villegas <1467833444@mil>
2018-02-05 09:45:36 -0500

    regex added to tshark
    initial addition to search for flags. However, still working on debugging

--- tShark.py
+++ tShark.py
@@ -1,18 +1,23 @@
 import subprocess
 import os
+import re
 
-work_dir = os.getcwd()
-tgt_dir = os.path.dirname(work_dir)
-pcap_dir = ''
-for dir_name in os.walk(tgt_dir):
-	if 'put_pcaps_here' == os.path.basename(dir_name[0]):
-		pcap_dir = dir_name[0]
+#work_dir = os.getcwd()
+#tgt_dir = os.path.dirname(work_dir)
+#pcap_dir = ''
+#for dir_name in os.walk(tgt_dir):
+#	if 'put_pcaps_here' == os.path.basename(dir_name[0]):
+#		pcap_dir = dir_name[0]
+#		print('here')
 
-for fil in os.listdir(pcap_dir):
-	sname = os.fsdecode(fil)
-	fname = pcap_dir+"\\"+sname
+
+for fil in range(1): #os.listdir(pcap_dir):
+	sname = "test1.pcap"
+	fname = "C:\\Users\\x86075\\Documents\\GitHub\\ConstVig\\pcap_test_backups" + "\\"+sname #pcap_dir+"\\"+sname
 	fbase = sname.split(".")[0]
 	newf = fbase + ".csv"
+	r=r"^\w\t"
+	regx = re.compile(r)
 	if ".pcap" == sname[-5:]:
 		tsharkCall1 = [
 						"tshark",
@@ -28,6 +33,8 @@ for fil in os.listdir(pcap_dir):
 						"tshark",
 						"-r",
 						fname,
+						"-Y",
+						regx,
 						"-T",
 						"fields",
 						"-e",
@@ -47,8 +54,8 @@ for fil in os.listdir(pcap_dir):
 						"-E",
 						"occurrence=f",
 						">",
-						pcap_dir +"\\"+newf
+						"C:\\Users\\x86075\\Documents\\GitHub\\ConstVig\\pcap_test_backups" + "\\"+newf
 		]
 		process = subprocess.Popen(tsharkCall2, shell=True, stdout=subprocess.PIPE)
 		process.wait()
-		os.unlink(fname)
+#		os.unlink(fname)
