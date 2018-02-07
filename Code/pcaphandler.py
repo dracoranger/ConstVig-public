056698468f38811e278c2757c93b564de660590e
Kronenberg <1501767243@mil>
2018-02-07 11:40:43 -0500

    pcaphandler 2.0

--- Code/pcaphandler.py
+++ Code/pcaphandler.py
@@ -1,61 +1,62 @@
 import subprocess
 import os
-import datetime
+#import datetime
 
-work_dir = os.getcwd()
-tgt_dir = os.path.dirname(work_dir)
+work_dir = os.getcwd() #gets current directory
+tgt_dir = os.path.dirname(work_dir) #moves up one level
 pcap_dir = ''
 for dir_name in os.walk(tgt_dir):
+	#walks through all sister directories of current directory
 	if 'put_pcaps_here' == os.path.basename(dir_name[0]):
 		pcap_dir = dir_name[0]
 
 for fil in os.listdir(pcap_dir):
+	#splits up large pcaps into smaller pcaps inside folders, deletes large pcaps
 	sname = os.fsdecode(fil)
 	fname = pcap_dir+"\\"+sname
-	splitLocl = 'SplitCap.exe'
-	inp =  splitLocl +" -r " +fname = " -s session"
-	process = subprocess.Popen(inp, shell=True, stdout=subprocess.PIPE)
+	splitLocl = 'SplitCap' #is a path to SplitCap, not just a string.
+	inp =  splitLocl + " -r " + fname + " -s session"
+	process = subprocess.Popen(inp, stdout=subprocess.PIPE)
 	process.wait()
 	os.unlink(fname)
 
-for fil in os.listdir(pcap_dir):
-	#need to figure out how to iterate through subfolder
-	#nested for loop?
-	sname = os.fsdecode(fil)
-	fname = pcap_dir+"\\"+sname
-	fbase = sname.split(".")[0]
-	tname = datetime.datetime.now().strftime("%d_%H.%M.%S")
-	#potential issue of two files having the same name IF:
-	#1) the pcaps have the exact same name
-	#2) they are processed in the same second
-	#This is unlikely enough for us to accept the potential bug.
-	newf = fbase + "_" + tname + ".csv"
-	if ".pcap" == sname[-5:]:
-		tsharkCall = [
-						"tshark",
-						"-r",
-						fname,
-						"-T",
-						"fields",
-						"-e",
-						"frame.number",
-						"-e",
-						"frame.time_epoch",
-						"-e",
-						"tcp.srcport",
-						"-e",
-						"tcp.dstport",
-						"-E",
-						"header=y",
-						"-E",
-						"separator=,",
-						"-E",
-						"quote=d",
-						"-E",
-						"occurrence=f",
-						">",
-						pcap_dir +"\\"+newf
-		]
-		process = subprocess.Popen(tsharkCall, shell=True, stdout=subprocess.PIPE)
-		process.wait()
-		os.unlink(fname)
+for sub_dir in os.walk(pcap_dir):
+	for fil in os.listdir(sub_dir[0]):
+		#need to figure out how to iterate through subfolder
+		#nested for loop?
+		sname = os.fsdecode(fil)
+		fname = sub_dir[0] +"\\"+sname
+		fbase = sname.split(".")[0] #file name without '.pcap'
+		#tname = datetime.datetime.now().strftime("%d_%H.%M.%S")
+		newf = fbase + ".csv"
+		if ".pcap" == sname[-5:]:
+			tsharkCall = [
+							"tshark",
+							"-r",
+							fname,
+							"-Y",
+							"frame contains 31:41:47:33:35:39:54:",
+							"-T",
+							"fields",
+							"-e",
+							"frame.number",
+							"-e",
+							"frame.time_epoch",
+							"-e",
+							"tcp.srcport",
+							"-e",
+							"tcp.dstport",
+							"-E",
+							"header=y",
+							"-E",
+							"separator=,",
+							"-E",
+							"quote=d",
+							"-E",
+							"occurrence=f",
+							">",
+							sub_dir[0] +"\\"+newf
+			]
+			process = subprocess.Popen(tsharkCall, shell=True, stdout=subprocess.PIPE)
+			process.wait()
+			#os.unlink(fname) #***
