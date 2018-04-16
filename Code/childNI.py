2647840f6d456911781e9a1688423619cf5dd6a9
Tate <dracoranger@hotmail.com>
2018-04-16 09:55:03 -0400

    Code merged
    JarynCode removed

--- Code/childNI.py
+++ Code/childNI.py
@@ -12,12 +12,12 @@
 #     search_sql_flows_port_out
 #     search_sql
 #     getcur
-#     print_sql_datbase
+#     print_sql_database
 
 import socket
 import sys
 import os
-
+import time
 import csv
 import sqlite3
 
@@ -32,66 +32,83 @@ import pcaphandler
 
 
 def main():
-    # Checks for type and returns the input,
-    # otherwise returns an error message
-    # Here is where database stuff begins
+    s = time.time()
+    cwd = os.getcwd()
+
     config = utilities.parse_config('NetworkIn')
+    pcap_dir = config['pcapfolder']
     regex = config['regex']
     length = config['length']
 
-    db_already_made = False
-    if 'packets.db' in os.listdir():
-        db_already_made = True
-    conn = sqlite3.connect('packets.db')
-    cur = conn.cursor()
-    if not db_already_made:
-        generate_db(cur)
+    regex= regex.format(length)[1:-1]
 
-    work_dir = os.getcwd()
-    tgt_dir = os.path.dirname(work_dir)
-    pcap_dir = ''
+                                # db_already_made = False
+                                # if 'packets.db' in os.listdir():
+                                #     db_already_made = True
+                                # conn = sqlite3.connect('packets.db')
+                                # cur = conn.cursor()
+                                # if not db_already_made:
+                                #     generate_db(cur)
 
-    for dir_name in os.walk(tgt_dir):
-        if 'put_pcaps_here' == os.path.basename(dir_name[0]):
-            pcap_dir = dir_name[0]
-    numberChanged = pcaphandler.split()
+    numberChanged = pcaphandler.split(pcap_dir)
     # Pcaps in put pcaps here should be moved or deleted
     # Will start with moved
     # if there is more than one file, move it to processed
     # only moves files
+    #TODO OS changes in pcaphandler
     if numberChanged > 0:
-        for i in os.listdir(pcap_dir):
-            if os.path.isfile(i):
-                os.rename(pcap_dir+'\\'+i,os.getcwd()+'\\processed\\'+i)
-    flows = pcaphandler.get_sql_data(regex, length)
-    for dir_name in os.listdir(pcap_dir):
-        if os.path.isdir(dir_name) and not dir_name == "processed":
-            os.rename(pcap_dir+'\\'+dir_name,pcap_dir+'\\processed\\'
-                      +dir_name)
-    flowsCSV = flows[0]
-    addpacket(flowsCSV, conn, cur)
-    os.chdir('C:\\Users\\T\\Documents\\GitHub\\ConstVig\\Code')
-    print_sql_database()
-
+        os.chdir(pcap_dir)
+        for fil in os.listdir(pcap_dir):
+            if os.path.isfile(fil):
+                os.rename(pcap_dir+'\\'+fil,pcap_dir+'\\PROCESSED\\'+fil)
+        os.chdir(cwd)
+    scan = pcaphandler.get_sql_data(regex,pcap_dir)
+    #DATABASE STARTS HERE
+    os.chdir(cwd)
+    if 'packets.db' not in os.listdir(cwd):
+        conn = sqlite3.connect('packets.db')
+        cur = conn.cursor()
+        generate_db(conn)
+    os.chdir(pcap_dir)
+    for sub_dir in os.listdir(os.getcwd()):
+        os.chdir(pcap_dir)
+        if sub_dir == 'PROCESSED':
+            continue
+        os.chdir(sub_dir)
+        try:
+            addpacket(sub_dir+'.csv',cwd)
+            os.chdir(pcap_dir)
+            os.rename(os.getcwd()+'\\'+sub_dir+'\\'+sub_dir+'.csv',os.getcwd()+'\\'+sub_dir+'\\PROCESSED\\'+sub_dir+'.csv')
+        except FileNotFoundError:
+            print('File has been moved ')
+    print_sql_database(cwd)
+    e=time.time()
+    print("Total time: ", e-s)
+    return "Analyzer is complete"
 
 def generate_db(conn):
     conn.execute('''CREATE TABLE flows
-             (flowNum integer primary key,
-              timest real,
+             (id integer primary key UNIQUE,
+              timest text not NULL,
+              srcIP text not NULL,
+              dstIP text not NULL,
               portIn integer,
               portOut integer,
-              flowReference text)''')
+              flag text not NULL,
+              flowReference text not NULL)''')
+    conn.commit()
 
-def addpacket(flow, conn, cur):
-    with open(flow, 'r+') as flo:
+def addpacket(flow,cwd):
+    with open(flow, 'r') as flo:
         lines = flo.readlines()
+    os.chdir(cwd)
+    conn = sqlite3.connect('packets.db')
+    cur = conn.cursor()
     data_groups = []
     for i in lines:
-        fields = i.split(',')
-        temp = (fields[0], fields[1], fields[2], fields[3])
-        data_groups.append(temp)
-    for line in range(0, len(data_groups)):
-        cur.execute("""INSERT INTO flows(timest, portIn, portOut, flowReference) VALUES (?, ?, ?, ?)""", data_groups[line])
+        fields = i.strip().split(',')
+        cur.execute("""INSERT INTO flows(timest,srcIP,dstIP,portIn,portOut,flag,flowReference) VALUES (?,?,?,?,?,?,?)""",
+        (fields[0],fields[1],fields[2],fields[3],fields[4],fields[5],fields[6]))
     conn.commit()
 
 # finds flows with a given port in
@@ -116,13 +133,14 @@ def getcur():
     return conn.cursor()
 
 
-def print_sql_database():
+def print_sql_database(cwd):
+    os.chdir(cwd)
     conn = sqlite3.connect('packets.db')
     cur = conn.cursor()
     #These were used to recreate the database
     # flow='testDB.csv'
     # addpacket(flow,conn,sql)
     flows = search_sql("""SELECT * FROM flows""", cur)
-    with open('flows.csv', 'w+') as fCSV:
+    with open('flows.csv', 'w+', newline='') as fCSV:
         writer = csv.writer(fCSV)
         writer.writerows(flows)
